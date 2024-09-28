db1b4c9936289c164b2823ecee21a64a0c981b69
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-09-28 13:56:11 +0900

    Delete module_2_5.py

--- module_2_5.py
+++
@@ -1,8 +0,0 @@
-def get_matrix(n, m, value):
-    matrix = []
-    [matrix.append([value] * m) for _ in range(n)]
-    return matrix
-
-while True:
-    a, b, c = int(input("Введите количество строк: ")), int(input("Введите количество столбцов: ")), int(input("Введите значение: "))
-    print(*get_matrix(a, b, c))
