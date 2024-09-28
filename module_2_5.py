7c8d53ce01f0fcfce93c1c57c754d2b7aff46a53
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-09-28 18:46:03 +0900

    "Матрица воплоти"
    Домашняя работа по уроку "Функции в Python.Функция с параметром"

--- module_2_5.py
+++ module_2_5.py
@@ -1,11 +1,3 @@
-# Объявите функцию get_matrix и напишите в ней параметры n, m и value.
-# Создайте пустой список matrix внутри функции get_matrix.
-# Напишите первый(внешний) цикл for для кол-ва строк матрицы, n повторов.
-# В первом цикле добавляйте пустой список в список matrix.
-# Напишите второй(внутренний) цикл for для кол-ва столбцов матрицы, m повторов.
-# Во втором цикле пополняйте ранее добавленный пустой список значениями value.
-# После всех циклов верните значение переменной matrix.
-# Выведите на экран(консоль) результат работы функции get_matrix.
 # 1
 def get_matrix(n, m, value):
     matrix = []
@@ -16,8 +8,12 @@ def get_matrix(n, m, value):
     return matrix
 
 while True:
-    a, b, c = int(input("Введите количество строк: ")), int(input("Введите количество столбцов: ")), input("Введите значение: ")
-    print(get_matrix(a, b, c))
+    a, b, c = int(input("Введите количество строк: ")), int(input("Введите количество столбцов: ")), int(input("Введите значение: "))
+    if c <= 0:
+        print([])
+    else:
+        print(get_matrix(a, b, c))
+
 
 # 2
 
@@ -27,5 +23,8 @@ def get_matrix(n, m, value):
     return matrix
 
 while True:
-    a, b, c = int(input("Строк: ")), int(input("Столбцов: ")), input("Значение: ")
-    print(get_matrix(a, b, c))
+    a, b, c = int(input("Строк: ")), int(input("Столбцов: ")), int(input("Значение: "))
+    if c <= 0:
+        print([])
+    else:
+        print(get_matrix(a, b, c))
